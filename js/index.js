a4fe73d01bd47490ffc7affa6794930f37a4c559
12144 <1214425528@qq.com>
2019-06-06 21:13:39 +0800

    添加填写信息的弹窗

---
+++ js/index.js
@@ -0,0 +1,6 @@
+function showFillInfo(){
+    document.getElementById("bcFillInfoMask").style.visibility="visible"//显
+}
+function hideFillInfo(){
+    document.getElementById("bcFillInfoMask").style.visibility="hidden"//显
+}
