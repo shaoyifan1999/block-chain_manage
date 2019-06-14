6bb9dd8e76b3d77b4af0b833f3d1f3da069e4f70
unknown <lurongfu@buaa.edu.cn>
2019-06-14 14:12:30 +0800

    完成三个页面

--- js/index.js
+++
@@ -1,6 +0,0 @@
-function showFillInfo(){
-    document.getElementById("bcFillInfoMask").style.visibility="visible"//显
-}
-function hideFillInfo(){
-    document.getElementById("bcFillInfoMask").style.visibility="hidden"
-}
